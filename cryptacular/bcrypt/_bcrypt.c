30b80015d79e01d299e4a45538a31fd4659cacba
Frank Smit <frank@61924.nl>
2011-08-18 00:05:56 +0200

    _bcrypt.c now compiles for Python too.

--- cryptacular/bcrypt/_bcrypt.c
+++ cryptacular/bcrypt/_bcrypt.c
@@ -24,69 +24,145 @@
 #include <Python.h>
 #include "ow-crypt.h"
 
-static PyObject *_py_crypt_rn(PyObject *self, PyObject *args) {
-	char *rc;
-	const char *key;
-	const char *setting;
-	char output[61];
 
-	memset(output, 0, sizeof(output));
-
-	if (!PyArg_ParseTuple(args, "ss", &key, &setting)) {
-		return NULL;
-	}
-
-	Py_BEGIN_ALLOW_THREADS;
-	
-	/* key, setting, output, size */
-	rc = crypt_rn(key, setting, output, sizeof(output));
-
-	Py_END_ALLOW_THREADS;
-
-	if (rc == NULL) {
-		Py_RETURN_NONE;
-	}
-
-	output[sizeof(output) - 1] = '\0';
-
-	return Py_BuildValue("s", output);
-}
-
-static PyObject *_py_crypt_gensalt_rn(PyObject *self, PyObject *args) {
-	char *rc;
-	const char *prefix;
-	const int count;
-	const char *salt;
-	const Py_ssize_t salt_len;
-	char output[30];
-
-	memset(output, 0, sizeof(output));
-	
-	if (!PyArg_ParseTuple(args, "sis#", &prefix, &count, &salt, &salt_len)) {
-		return NULL;
-	}
-
-	/* prefix, count, input, size, output, output_size */
-	rc = crypt_gensalt_rn(prefix, count, salt, salt_len, output, sizeof(output));
-
-	if (rc == NULL) {
-		Py_RETURN_NONE;
-	}
-
-	output[sizeof(output) - 1] = '\0';
-
-	return Py_BuildValue("s", output);
-}
-
-static PyMethodDef BcryptMethods[] = {
-	{"crypt_rn", _py_crypt_rn, METH_VARARGS, "Encrypt password"},
-	{"crypt_gensalt_rn", _py_crypt_gensalt_rn, METH_VARARGS, "Generate salt"},
-	{NULL, NULL, 0, NULL}
+struct module_state {
+    PyObject *error;
 };
 
-PyMODINIT_FUNC
-init_bcrypt(void)
+
+#if PY_MAJOR_VERSION >= 3
+    #define GETSTATE(m) ((struct module_state*)PyModule_GetState(m))
+#else
+    #define GETSTATE(m) (&_state)
+    static struct module_state _state;
+#endif
+
+
+static PyObject *
+_py_crypt_rn(PyObject *self, PyObject *args)
 {
-	(void) Py_InitModule("_bcrypt", BcryptMethods);
+    char *rc;
+    const char *key;
+    const char *setting;
+    char output[61];
+
+    memset(output, 0, sizeof(output));
+
+    if (!PyArg_ParseTuple(args, "ss", &key, &setting)) {
+        return NULL;
+    }
+
+    Py_BEGIN_ALLOW_THREADS;
+
+    /* key, setting, output, size */
+    rc = crypt_rn(key, setting, output, sizeof(output));
+
+    Py_END_ALLOW_THREADS;
+
+    if (rc == NULL) {
+        Py_RETURN_NONE;
+    }
+
+    output[sizeof(output) - 1] = '\0';
+
+    return Py_BuildValue("s", output);
+}
+
+
+static PyObject *
+_py_crypt_gensalt_rn(PyObject *self, PyObject *args)
+{
+    char *rc;
+    const char *prefix;
+    const int count;
+    const char *salt;
+    const Py_ssize_t salt_len;
+    char output[30];
+
+    memset(output, 0, sizeof(output));
+
+    if (!PyArg_ParseTuple(args, "sis#", &prefix, &count, &salt, &salt_len)) {
+        return NULL;
+    }
+
+    /* prefix, count, input, size, output, output_size */
+    rc = crypt_gensalt_rn(prefix, count, salt, salt_len, output, sizeof(output));
+
+    if (rc == NULL) {
+        Py_RETURN_NONE;
+    }
+
+    output[sizeof(output) - 1] = '\0';
+
+    return Py_BuildValue("s", output);
+}
+
+
+static PyMethodDef _bcrypt_methods[] = {
+    {"crypt_rn", _py_crypt_rn, METH_VARARGS, "Encrypt password"},
+    {"crypt_gensalt_rn", _py_crypt_gensalt_rn, METH_VARARGS, "Generate salt"},
+    {NULL, NULL, 0, NULL} /* Sentinel */
+};
+
+
+
+#if PY_MAJOR_VERSION >= 3
+    static int
+    _bcrypt_traverse(PyObject *m, visitproc visit, void *arg)
+    {
+        Py_VISIT(GETSTATE(m)->error);
+        return 0;
+    }
+
+    static int
+    _bcrypt_clear(PyObject *m)
+    {
+        Py_CLEAR(GETSTATE(m)->error);
+        return 0;
+    }
+
+    static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "_bcrypt",
+        NULL,
+        sizeof(struct module_state),
+        _bcrypt_methods,
+        NULL,
+        _bcrypt_traverse,
+        _bcrypt_clear,
+        NULL
+    };
+
+    #define INITERROR return NULL
+
+    PyObject *
+    PyInit__bcrypt(void)
+#else
+    #define INITERROR return
+
+    PyMODINIT_FUNC
+    init_bcrypt(void)
+#endif
+{
+    #if PY_MAJOR_VERSION >= 3
+        PyObject *module = PyModule_Create(&moduledef);
+    #else
+        PyObject *module = Py_InitModule("_bcrypt", _bcrypt_methods);
+    #endif
+
+    if (module == NULL) {
+        INITERROR;
+    }
+    struct module_state *st = GETSTATE(module);
+
+    st->error = PyErr_NewException("_bcrypt.Error", NULL, NULL);
+    if (st->error == NULL) {
+        Py_DECREF(module);
+        INITERROR;
+    }
+
+    #if PY_MAJOR_VERSION >= 3
+        return module;
+    #endif
 }
 
